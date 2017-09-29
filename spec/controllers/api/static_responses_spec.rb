4ba0aebc673d52d98e53c65272a71aeb00fae08e
Asnad Atta <asnadatta@gmail.com>
2017-09-29 14:58:49 +0500

    more test

--- spec/controllers/api/static_responses_spec.rb
+++ spec/controllers/api/static_responses_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Api::StaticResponsesController, :type => :controller do
       hash_body = nil
       get :home, format: :json
       expect(response.status).to eq(200)
+    end
+
+    it "home api response" do
+      hash_body = nil
+      get :home, format: :json
       expect { hash_body = JSON.parse(response.body).with_indifferent_access }.not_to raise_exception
       expect(hash_body).to match({
         message: 'Hello World'
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  it "should be important message" do
-  	message = create(:message, :important)
-  	binding.pry
+  describe "Valid Message" do
+    before(:all) do
+      @message = Message.new(text: "Hello Test")
+    end
+    it "valid message" do
+      expect(@message).to be_valid
+    end
+  end
+
+  describe "Invalid Message" do
+    before(:all) do
+      @message = Message.new()
+    end
+    it "invalid message" do
+      expect(@message).to be_invalid
+    end
   end
 end
